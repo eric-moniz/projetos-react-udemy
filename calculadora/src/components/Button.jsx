5ae55719f00d10b1c1102a6f29d7fd54d178c6a4
Eric Moniz <ericmoniz@outlook.com.br>
2023-03-20 22:25:47 -0300

    aula 48 - Componente Button #02

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 import './Button.css';
 
 const Button = props => {
-  return <button className='button'>{props.label}</button>;
+  let classes = `button `;
+  classes += props.operation ? 'operation' : '';
+  classes += props.double ? 'double' : '';
+  classes += props.triple ? 'triple' : '';
+
+  return (
+    <button
+      className={classes}
+      onClick={e => props.click && props.click(props.label)}
+    >
+      {props.label}
+    </button>
+  );
 };
 
 export default Button;
