f62b0b6dd6c22746d2602cbd59ebd04804671842
Eric Moniz <ericmoniz@outlook.com.br>
2023-03-21 21:23:32 -0300

    aula 55 - React Router Dom v6

--- navegacao/src/components/layout/Content.jsx
+++ navegacao/src/components/layout/Content.jsx
@@ -1,11 +1,22 @@
-import React from "react";
 import "./Content.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import About from "../../views/examples/About";
+import Param from "../../views/examples/Param";
+import Home from "../../views/examples/Home";
+import NotFound from "../../views/examples/NotFound";
 
 const Content = props => {
   return (
-    <div className="Content">
-      <h1>Componente Content</h1>
-    </div>
+    <main className="Content">
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/param:id" element={<Param />} />
+        <Route path="/" exact element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </main>
   );
 };
 
