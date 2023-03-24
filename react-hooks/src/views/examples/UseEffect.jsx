961d2a0702c481bac4ac874b90aef9b085516562
Eric Moniz <ericmoniz@outlook.com.br>
2023-03-24 18:51:20 -0300

    aula 67 - Desafio useEffect - Resposta

--- react-hooks/src/views/examples/UseEffect.jsx
+++ react-hooks/src/views/examples/UseEffect.jsx
@@ -10,9 +10,17 @@ function calcFatorial(num) {
   return calcFatorial(n - 1) * n
 }
 
+function isPar(num) {
+  if (num < 0) return
+  return num % 2 === 0
+}
+
 const UseEffect = props => {
   const [number, setNumber] = useState(1)
   const [fatorial, setFatorial] = useState(1)
+
+  const [num, setNum] = useState(0)
+  const [par, setPar] = useState(true)
 
   useEffect(() => {
     setFatorial(calcFatorial(number))
@@ -22,9 +30,13 @@ const UseEffect = props => {
     if (fatorial > 1000000) {
       document.title = "Eita!!!!"
     } else {
-      document.title = "Fatorial"
+      document.title = "React App"
     }
   })
+
+  useEffect(() => {
+    setPar(isPar(num))
+  }, [num])
 
   return (
     <div className="UseEffect">
@@ -34,7 +46,7 @@ const UseEffect = props => {
       />
 
       <SectionTitle title="Exercicio #01" />
-      <div className="center">
+      <div className="center mt">
         <div>
           <span className="text">Fatorial: </span>
           <span className="text red">
@@ -50,8 +62,25 @@ const UseEffect = props => {
         />
       </div>
 
+      {/* Desafio numero par ou impar */}
       <SectionTitle title="Exercício #02" />
-      <div className="center"></div>
+      <div className="center mt">
+        <div>
+          <span className="text">Status: </span>
+          <span className="text red">{par === true ? "Par" : "Ímpar"}</span>
+        </div>
+
+        <input
+          type="number"
+          className="input"
+          value={num}
+          onChange={e => {
+            if (e.target.value < 0) e.target.value = ""
+            setNum(e.target.value)
+          }}
+          min="0"
+        />
+      </div>
     </div>
   )
 }
