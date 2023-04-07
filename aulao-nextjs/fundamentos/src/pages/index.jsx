9d49d389069ab23c98ad97145ec3264103782ee6
Eric Moniz <ericmoniz@outlook.com.br>
2023-04-07 11:32:28 -0300

    aula 89 - Integração JavaScript/JSX

--- aulao-nextjs/fundamentos/src/pages/index.jsx
+++ aulao-nextjs/fundamentos/src/pages/index.jsx
@@ -1,8 +1,8 @@
 export default function inicio() {
-  return (
-    <div>
-      <h1>Fundamentos de Next.js & React</h1>
-      <h2>Vamos estudar esse framework</h2>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Fundamentos de Next.js & React</h1>
+            <h2>Vamos estudar esse framework</h2>
+        </div>
+    );
 }
---
+++ aulao-nextjs/fundamentos/src/pages/jsx.jsx
@@ -0,0 +1,15 @@
+export default function Jsx() {
+    const titulo = <h1>JSX é um conceito Central</h1>;
+
+    function subtitulo() {
+        return <h2>{'muito legal'.toUpperCase()}</h2>;
+    }
+
+    return (
+        <div>
+            {titulo}
+            {subtitulo()}
+            <p>{JSON.stringify({ nome: 'Joâo', idade: 30 })}</p>
+        </div>
+    );
+}
